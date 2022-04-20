db5e6597843b4b3777f0c822f89b62c2385764f2
JoshDreben <JoshDreben@users.noreply.github.com>
2022-04-20 15:13:54 -0700

    php-env

---
+++ josh-cse135.site/public_html/cgi-bin/php-env.php
@@ -0,0 +1,20 @@
+<?php
+
+header('Cache-Control: no-cache, must-revalidate');
+
+?>
+
+<html>
+<head>
+  <title>PHP Environment Variables by JOSH!</title>
+</head>
+
+<body>
+  <h1>PHP Environment Variables by JOSH!</h1>
+  <p>
+  <?php
+    echo phpinfo(); 
+  ?>
+  </p>
+</body
+</html>
