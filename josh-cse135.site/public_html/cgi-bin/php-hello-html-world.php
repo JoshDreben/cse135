c1bf70a14266c266a596f6d420adb609e55fd51b
JoshDreben <JoshDreben@users.noreply.github.com>
2022-04-20 15:02:10 -0700

    test PHP

---
+++ josh-cse135.site/public_html/cgi-bin/php-hello-html-world.php
@@ -0,0 +1,16 @@
+<?php 
+  header('Cache-Control: no-cache, must-revalidate')
+?>
+
+
+<html>
+<head>
+  <title>PHP Hello World! (by JOSH!)</title>
+</head>
+
+<body>
+  <h1>PHP Hello World! (by JOSH!)</title>
+  <p>Current Date/Time: <?php echo date('d-m-y h:i:s')?></p>
+  <p>IP Address: <?php echo $_SERVER['REMOTE_ADDR'] ?></p>
+</body>
+</html>
