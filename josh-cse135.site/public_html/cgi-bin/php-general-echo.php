59735a312fc876aef20c8e487275a75bb08b3f1f
JoshDreben <JoshDreben@users.noreply.github.com>
2022-04-20 16:35:37 -0700

    php-general-echo

---
+++ josh-cse135.site/public_html/cgi-bin/php-general-echo.php
@@ -0,0 +1,28 @@
+<?php
+header('Cache-Control: no-cache, must-revalidate');
+
+$query = $_SERVER['QUERY_STRING'];
+
+?>
+<html>
+<head>
+	<title>PHP General Request Echo by JOSH!</title>
+</head>
+<body>
+	<h1>PHP General Request Echo by JOSH!</h1>
+	<p>Protocol: <?php  echo getenv('SERVER_PROTOCOL') ?></p>
+	<p>Request Method: <?php  echo getenv('REQUEST_METHOD') ?></p>
+	<p>Query String: <?php  echo $query ?></p>
+	<p> Message: Body:</p>
+	<?php
+		foreach ($_GET as $key => $value) {
+			echo "<p>{$key}: {$value}</p>";
+		}
+	?>
+	<?php
+		foreach ($_POST as $key => $value) {
+			echo "<p>{$key}: {$value}</p>";
+		}
+	?>
+</body>
+</html>
