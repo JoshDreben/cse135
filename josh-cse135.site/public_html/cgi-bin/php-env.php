ca4a611cb3a13e7b9a4b653ac709c7d69464efbd
JoshDreben <JoshDreben@users.noreply.github.com>
2022-04-20 15:39:23 -0700

    php-env

--- josh-cse135.site/public_html/cgi-bin/php-env.php
+++ josh-cse135.site/public_html/cgi-bin/php-env.php
@@ -12,22 +12,14 @@ $envv = getenv();
 
 <body>
 
+  <h1>PHP Environment Variables by JOSH!</h1>
+  <h2>Environment Variables:<h2>
   <?php
   foreach ($envv as $key => $value) {
 	echo "<p>{$key}: {$value}</p>";
   }
   ?>
-  <!-- <h1>PHP Environment Variables by JOSH!</h1>
-  <p>APACHE_RIN_DIR: <?php echo getenv('APACHE_RUN_DIR');?></p>
-  <p>APACHE_PID_FILE: <?php echo getenv('APACHE_PID_FILE');?></p>
-  <p>JOURNAL_STREAM: <?php echo getenv('JOURNAL_STREAM');?></p>
-  <p>PATH: <?php echo getenv('PATH');?></p>
-  <p>INVOCATION_ID: <?php echo getenv('INVOCATION_ID');?></p>
-  <p>APACHE_LOCK_DIR: <?php echo getenv('APACHE_LOCK_DIR');?></p>
-  <p>LANG: <?php echo getenv('LANG');?></p>
-  <p>APACHE_RUN_USER: <?php echo getenv('APACHE_RUN_USER');?></p>
-  <p>APACHE_LOG_DIR: <?php echo getenv('APACHE_LOG_DIR');?></p>
-  <p>PWD: <?php echo getenv('PWD');?></p> -->
+  <h2>Server Variables:<h2>
   <?php 
   foreach ($_SERVER  as $key => $value) {
 	  echo "<p>{$key}: {$value}</p>";
