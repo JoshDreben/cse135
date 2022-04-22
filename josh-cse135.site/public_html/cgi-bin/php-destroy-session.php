241dc769140800bd8c11034818038b37c98f413d
JoshDreben <JoshDreben@users.noreply.github.com>
2022-04-22 15:43:56 -0700

    php sessions

---
+++ josh-cse135.site/public_html/cgi-bin/php-destroy-session.php
@@ -0,0 +1,14 @@
+<?php
+	header('Content-type: text/html');
+	session_destroy();
+?>
+
+<html>
+<head>
+	<title>PHP Session Destroyed by JOSH!</title>
+</head>
+<body>
+	<h1>PHP Session Destroyed by JOSH!</h1>
+	<a href="/php-cgiform.html">Back to the PHP Form!</a>
+</body>
+</html>
