c33103af1514e8b3a9e32d53a137aefb42ec8711
JoshDreben <JoshDreben@users.noreply.github.com>
2022-04-23 00:02:32 -0700

    py env

--- josh-cse135.site/public_html/cgi-bin/py-env.py
+++ josh-cse135.site/public_html/cgi-bin/py-env.py
@@ -11,6 +11,6 @@ print ("</head>")
 print ("<body>")
 print ("<h1>Python Environment by JOSH!</h1><\br>")
 for param in os.environ.keys():
-   print ("<b>%20s</b>: %s<\br>" % (param, os.environ[param]))
+   print ("<b>%20s</b>: %s<br>" % (param, os.environ[param]))
 print ("</body>")
 print ("</html>")
