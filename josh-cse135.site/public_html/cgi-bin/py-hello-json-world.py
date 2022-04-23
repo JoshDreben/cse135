45565bd7f7a775f77347269b774669b85b92ff44
JoshDreben <JoshDreben@users.noreply.github.com>
2022-04-22 23:44:19 -0700

    py json

--- josh-cse135.site/public_html/cgi-bin/py-hello-json-world.py
+++ josh-cse135.site/public_html/cgi-bin/py-hello-json-world.py
@@ -5,4 +5,5 @@ import os
 import html
 
 print ("Content-type:application/json\r\n\r\n")
-print ('{"title": "Python JSON by JOSH!", "message": "Hello from PYTHON JSON by JOSH!", "Date": "' + str(datetime.now()) + '", "IP Address": "'+str(html.escape(os.environ["REMOTE_ADDR"]+'"}')))
+
+print ('{"title": "Python JSON by JOSH!", "message": "Hello from PYTHON JSON by JOSH!", "Date": "' + str(datetime.now()) + '", "IP Address": "'+str(os.environ["REMOTE_ADDR"])+'"}')
