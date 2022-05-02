6333f7b25c5c0739b0bca0a3ceb194b8b5258ff7
JoshDreben <JoshDreben@users.noreply.github.com>
2022-05-02 16:15:33 -0700

    more image detection fix

--- josh-cse135.site/public_html/collector.js
+++ josh-cse135.site/public_html/collector.js
@@ -5,9 +5,9 @@ var activity = {};
 
 function imagesEnabled() {
 	if ((document.getElementById('imageFlag').offsetWidth == 1)) {
-		return true;
+		static["imagesEnabled"] = true;
 	} else {
-		return false;
+		static["imagesEnabled"] = false;
 	}
 }
 
@@ -15,7 +15,6 @@ function load() {
 	static["userAgent"] = window.navigator.userAgent;
 	static["language"] = window.navigator.language;
 	static["acceptCookies"] = window.navigator.cookieEnabled;
-	static["imagesEnabled"] = imagesEnabled();
 	static["windowInnerWidth"] = window.innerWidth;
 	static["windowInnerHeight"] = window.innerHeight;
 	static["windowOuterWidth"] = window.outerWidth;
