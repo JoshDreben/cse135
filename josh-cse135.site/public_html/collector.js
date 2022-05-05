422820ad984250c2f46c839169b9c7b274d505b8
JoshDreben <JoshDreben@users.noreply.github.com>
2022-05-05 14:14:03 -0700

    collector

--- josh-cse135.site/public_html/collector.js
+++ josh-cse135.site/public_html/collector.js
@@ -19,12 +19,9 @@ function showNavDetails() {
 	// Get the first entry
 	const po = new PerformanceObserver((list) => {
 		for (const entry of list.getEntries()) {
-		  // Log the entry and all associated details.
 		  console.log(entry.toJSON());
 		}
 	  });
-	
-	po.observe({type: 'some-entry-type'});
 }
 
 function load() {
