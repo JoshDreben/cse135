2a4388a0e745d53f5addb3c18daa6d8a6dd76954
JoshDreben <JoshDreben@users.noreply.github.com>
2022-05-07 14:57:34 -0700

    REST

--- josh-cse135.site/public_html/api/static.php
+++ josh-cse135.site/public_html/api/static.php
@@ -2,11 +2,7 @@
 	header('Content-type: application/json');
 	$json_res = NULL;
 	if($_SERVER['REQUEST_METHOD'] === 'POST') {
-		foreach ($_POST as $key => $value)
-		{
-			//$json_res[$key] = $value;	
-			$json_res["METHOD"] = "POST";
-	    }
+		$json_res["METHOD"] = 'POST';
 	}
 	// } else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 	// 	$json_res = json_encode($_REQUEST);
