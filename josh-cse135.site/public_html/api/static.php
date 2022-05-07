552c968b7cffb3baa4b56cd364e889ea4e943778
JoshDreben <JoshDreben@users.noreply.github.com>
2022-05-07 15:37:11 -0700

    REST

--- josh-cse135.site/public_html/api/static.php
+++ josh-cse135.site/public_html/api/static.php
@@ -19,9 +19,8 @@
 			echo json_encode($json_res);
 			exit();
 		}
-		$json_res = json_encode($decoded);
+		$json_res = $decoded;
 		file_put_contents("test_db.json", $json_res);
-
 	} else if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_GET["id"])) {
 		// POST REQUEST ON OLD RECORD (not allowed)
 		http_response_code(400);
