b9e23d76a45a4949959abbe109b95f77f6a77578
JoshDreben <JoshDreben@users.noreply.github.com>
2022-05-07 15:06:32 -0700

    REST

--- josh-cse135.site/public_html/api/static.php
+++ josh-cse135.site/public_html/api/static.php
@@ -4,12 +4,14 @@
 	if($_SERVER['REQUEST_METHOD'] === 'POST') {
 		$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 		if(strcasecmp($contentType, 'application/json') != 0){
-			throw new Exception('Content type must be: application/json!');
+			http_response_code(400);
+			exit("Content type must be application/json!");
 		}
 		$content = trim(file_get_contents("php://input"));
 		$decoded = json_decode($content, true);
 		if(!is_array($decoded)){
-			throw new Exception('Received content contained invalid JSON!');
+			http_response_code(400);
+			exit("JSON body is malformed!");
 		}
 
 		$json_res = $decoded;
