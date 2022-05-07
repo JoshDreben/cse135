e102f4cbc0ab3e500e71f85b8dabf537252231c7
JoshDreben <JoshDreben@users.noreply.github.com>
2022-05-07 15:08:47 -0700

    REST

--- josh-cse135.site/public_html/api/static.php
+++ josh-cse135.site/public_html/api/static.php
@@ -5,7 +5,9 @@
 		$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 		if(strcasecmp($contentType, 'application/json') != 0){
 			http_response_code(400);
-			exit("Content type must be application/json!");
+			$json_res["message"] = "Content type must be application/json!";
+			echo json_encode($json_res);
+			exit();
 		}
 		$content = trim(file_get_contents("php://input"));
 		$decoded = json_decode($content, true);
