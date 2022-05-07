46701128454322b926d21acd534da288dcbcbea8
JoshDreben <JoshDreben@users.noreply.github.com>
2022-05-07 15:03:45 -0700

    REST

--- josh-cse135.site/public_html/api/static.php
+++ josh-cse135.site/public_html/api/static.php
@@ -2,14 +2,17 @@
 	header('Content-type: application/json');
 	$json_res = NULL;
 	if($_SERVER['REQUEST_METHOD'] === 'POST') {
+		$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+		if(strcasecmp($contentType, 'application/json') != 0){
+			throw new Exception('Content type must be: application/json!');
+		}
+		$content = trim(file_get_contents("php://input"));
+		$decoded = json_decode($content, true);
+		if(!is_array($decoded)){
+			throw new Exception('Received content contained invalid JSON!');
+		}
 
-		$json_res["METHOD"] = 'POST';
+		$json_res = $decoded;
 	}
-	// } else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-	// 	$json_res = json_encode($_REQUEST);
-	// } 
-
-	$content = trim(file_get_contents("php://input"));
-	$decoded = json_decode($content, true);
-	echo json_encode($decoded);
+	echo json_encode($json_res);
 ?>
