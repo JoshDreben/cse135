2722659d56745f4dfa1e55f9913719d983d672a8
JoshDreben <JoshDreben@users.noreply.github.com>
2022-05-07 15:10:06 -0700

    REST

--- josh-cse135.site/public_html/api/static.php
+++ josh-cse135.site/public_html/api/static.php
@@ -1,5 +1,6 @@
 <?php
 	header('Content-type: application/json');
+	header('Cache-control: no-cache');
 	$json_res = NULL;
 	if($_SERVER['REQUEST_METHOD'] === 'POST') {
 		$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
@@ -13,9 +14,10 @@
 		$decoded = json_decode($content, true);
 		if(!is_array($decoded)){
 			http_response_code(400);
-			exit("JSON body is malformed!");
+			$json_res["message"] = "JSON was sent malformed!";
+			echo json_encode($json_res);
+			exit();
 		}
-
 		$json_res = $decoded;
 	}
 	echo json_encode($json_res);
