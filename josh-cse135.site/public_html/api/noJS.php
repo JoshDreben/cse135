608f13d607629522ce0fd0b852fd3e9c189075ca
JoshDreben <JoshDreben@users.noreply.github.com>
2022-05-10 15:00:16 -0700

    REST

--- josh-cse135.site/public_html/api/noJS.php
+++ josh-cse135.site/public_html/api/noJS.php
@@ -2,7 +2,8 @@
   header('Content-Type: text/plain');
   $con = mysqli_connect("localhost","admin","CSE135@dmin","cse135");
   $ip = $_SERVER["REMOTE_ADDR"];
-  $sql = "INSERT INTO no_js(ip_addr) VALUES('$ip')";
+  $time = $_SERVER["REQUEST_TIME"];
+  $sql = "INSERT INTO no_js(ip_addr, timestamp) VALUES('$ip','$time')";
   $res = $con->query($sql);
   echo "Enable javascript for full functionality of the website!";
 ?>
