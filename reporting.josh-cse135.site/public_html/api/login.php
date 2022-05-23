c92aecf6c1edc908343b268837e1effb52c2fa2e
JoshDreben <JoshDreben@users.noreply.github.com>
2022-05-23 16:14:14 -0700

    LOGIN

--- reporting.josh-cse135.site/public_html/api/login.php
+++ reporting.josh-cse135.site/public_html/api/login.php
@@ -1,5 +1,30 @@
 <?php
 	header('Content-Type: application/json');
-	$res["msg"] = "hello!";
-	echo json_encode($res);
+	header('Cache-Control: no-cache');
+	$res = NULL;
+	$con = mysqli_connect("db-mysql-sfo3-98726-do-user-11245821-0.b.db.ondigitalocean.com:25060","doadmin","AVNS_Q91stfvIS6ImiPP","cse135");
+	if (mysqli_connect_errno()){
+		http_response_code(500);
+		$json_res["message"] = "Failed to connect to MySQL: " . mysqli_connect_error();
+		echo json_encode($json_res);
+		exit();
+	}
+	$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+	if(strcasecmp($contentType, 'application/json') != 0){
+		mysqli_close($con);
+		http_response_code(400);
+		$json_res["message"] = "Content type must be application/json!";
+		echo json_encode($json_res);
+		exit();
+	}
+	$content = trim(file_get_contents("php://input"));
+	$decoded = json_decode($content, true);
+	if(!is_array($decoded)){
+		mysqli_close($con);
+		http_response_code(400);
+		$json_res["message"] = "JSON was sent malformed!";
+		echo json_encode($json_res);
+		exit();
+	}
+	echo json_encode($decoded);
 ?>
