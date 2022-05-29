5647e098a87cb4301191ef476ed36d6d7be46490
JoshDreben <JoshDreben@users.noreply.github.com>
2022-05-29 15:48:49 -0700

    DASH

--- reporting.josh-cse135.site/public_html/api/dash.php
+++ reporting.josh-cse135.site/public_html/api/dash.php
@@ -2,8 +2,8 @@
 	header('Content-Type: application/json');
 	header('Cache-Control: no-cache');
 	$json_res = NULL;
-	$mysqli = new mysqli("db-mysql-sfo3-98726-do-user-11245821-0.b.db.ondigitalocean.com:25060","doadmin","AVNS_Q91stfvIS6ImiPP","cse135");
-	if ($mysqli->connect_errno){
+	$con = mysqli_connect("db-mysql-sfo3-98726-do-user-11245821-0.b.db.ondigitalocean.com:25060","doadmin","AVNS_Q91stfvIS6ImiPP","cse135");
+	if (mysqli_connect_errno()){
 		http_response_code(500);
 		$json_res["message"] = "Failed to connect to MySQL: " . mysqli_connect_error();
 		echo json_encode($json_res);
@@ -19,26 +19,23 @@
 
 	$day_range = $_GET["day_range"];
 	$sql = "CALL call_dashboard($day_range)";
-	$stmt = $mysqli->prepare($sql);
-	$stmt->execute();
+	$con->multi_query($sql);
+	$res = $con->store_result();
 	$emparray = array();
-	$res = $mysqli->use_result();
 	while ($row = mysqli_fetch_assoc($res))
 	{
 		$emparray[] = $row;
-		$res->close();
 	}
 	$dashboard = $emparray;
 	$json_res["dash"] = $dashboard;
-	$emparray = arrary();
-	$res = $con->use_result();
+	$con->next_result();
+	$res = $con->store_result();
+	$emparray = array();
 	while ($row = mysqli_fetch_assoc($res))
 	{
 		$emparray[] = $row;
-		$res->close();
 	}
 	$activity = $emparray;
 	$json_res["activity"] = $activity;
-	$mysqli->close();
 	echo json_encode($json_res);
 ?>
